fda1ab5a754c5e7e9334fbf9f60b9cfc4363d10a
hanyu <hanyu2@asu.edu>
2017-05-30 11:59:15 -0700

    Fix footer style

--- client/components/includes/footer.js
+++ client/components/includes/footer.js
@@ -5,14 +5,18 @@ class Footer extends Component{
   render(){
     return(
       <footer className="footer text-center">
-        <div className="container">
-          <div>Powered by</div>
-          <div>
-            <img height="20px" src="/images/meteor-logo.svg"/>&nbsp;
-            <img height="20px" src="/images/react-logo.svg"/>
-            <p styles="color: #61dafb">React</p>
-          </div>
-        </div>
+          <div className="powered-by">Powered by</div>
+            <div className="logos">
+              <div className="meteor-logo">
+                <img height="20px" src="/images/meteor-logo.svg"/>&nbsp;&nbsp;
+              </div>
+              <div className="react-logo">
+                <img className="pic-down" height="20px" src="/images/react-logo.svg"/>
+                <text className="react-text">
+                  &nbsp;React
+                </text>
+              </div>
+            </div>
           <div className="container">
               <small className="copyright">Template Copyright @ <a href="http://themes.3rdwavemedia.com/" target="_blank">3rd Wave Media</a></small>
           </div>
