06d564533dc125f2ce34258847295c35737fe3a0
hanyu <hanyu2@asu.edu>
2017-05-30 00:04:58 -0700

    Add profile img

--- client/components/includes/footer.js
+++ client/components/includes/footer.js
@@ -5,6 +5,14 @@ class Footer extends Component{
   render(){
     return(
       <footer className="footer text-center">
+        <div className="container">
+          <div>Powered by</div>
+          <div>
+            <img height="20px" src="/images/meteor-logo.svg"/>&nbsp;
+            <img height="20px" src="/images/react-logo.svg"/>
+            <p styles="color: #61dafb">React</p>
+          </div>
+        </div>
           <div className="container">
               <small className="copyright">Template Copyright @ <a href="http://themes.3rdwavemedia.com/" target="_blank">3rd Wave Media</a></small>
           </div>
