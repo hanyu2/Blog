1b5833897e451ddb0d1fa6cd59d84a28da8dab5f
hanyu <hanyu2@asu.edu>
2017-05-31 23:46:43 -0700

    Render blog item

--- client/components/blogs/blogItem.js
+++ client/components/blogs/blogItem.js
@@ -1,26 +1,15 @@
 import React, {Component} from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
 import ReactDom from 'react-dom';
 import {Link} from 'react-router-dom';
-import {Posts} from '../../../imports/collections/posts.js';
 
-class BlogItem extends Component{
-  componentWillMount(){
-    Meteor.subscribe('posts');
-  }
-
-  render(){
-      console.log(this.props.posts);
-      return(
-        <div className="list-group-item">
-          test
-        </div>
-      );
-
-  };
+const BlogItem = ({post}) =>{
+  const { title } = post;
+  return(
+    <div className='list-group-item'>
+      title:{title};
+    </div>
+  );
 }
 
 
-export default createContainer(() => {
-  return { posts: Posts.find({}).fetch()};
-}, BlogItem);
+export default BlogItem;
--- client/components/blogs/blogList.js
+++ client/components/blogs/blogList.js
@@ -2,17 +2,24 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import {Link} from 'react-router-dom';
 import BlogItem from './blogItem.js';
+import { createContainer } from 'meteor/react-meteor-data';
+import {Posts} from '../../../imports/collections/posts.js';
+
 
 class BlogList extends Component{
   render(){
+    console.log(this.props.posts);
     return(
       <div>
         <ul className="list-group">
-            <BlogItem />
+            {this.props.posts.map(post => <BlogItem key={post._id} post={post}/>)}
         </ul>
       </div>
     );
   };
 }
 
-export default BlogList;
+export default createContainer((props) => {
+  Meteor.subscribe('posts')
+  return {posts: Posts.find({}).fetch() };
+}, BlogList);
