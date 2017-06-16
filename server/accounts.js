bf803a7d82f4ca603b69fb5a60079969a0e8d908
hanyu <hanyu2@asu.edu>
2017-06-16 12:20:18 -0700

    Fix google OAuth secret

--- server/accounts.js
+++ server/accounts.js
@@ -1,13 +1,9 @@
-const settings = Meteor.settings.google;
-console.log(settings);
-if (settings) {
-  ServiceConfiguration.configurations.remove({
-    service: 'google'
-  });
+ServiceConfiguration.configurations.remove({
+  service: 'google'
+});
 
-  ServiceConfiguration.configurations.insert({
-    service: 'google',
-    clientId: "117054743738-hqs7vkc1o7vn0aji6ehimkd4br3ff32a.apps.googleusercontent.com",
-    secret: "CguNZYjsNEWT_rgOlZndJYtS"
-  });
-}
+ServiceConfiguration.configurations.insert({
+  service: 'google',
+  clientId: "117054743738-hqs7vkc1o7vn0aji6ehimkd4br3ff32a.apps.googleusercontent.com",
+  secret: "CguNZYjsNEWT_rgOlZndJYtS"
+});
