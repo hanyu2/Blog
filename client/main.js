b2507d8daa82ddd43c2b52f98152e1f684503c59
hanyu <hanyu2@asu.edu>
2017-06-17 10:32:48 -0700

    Set ROOT_URL

--- client/main.js
+++ client/main.js
@@ -8,4 +8,9 @@ let store = createStore(BlogReducer)
 
 Meteor.startup(() => {
   ReactDOM.render(<Router store={store}/>, document.querySelector(".render-target"));
+  console.log(location.host);
+  process.env.ROOT_URL = 'http://blog.han-yu.me';
+  if (location.host.indexOf('blog.han-yu.me') !== 0) {
+        location = 'http://blog.han-yu.me'
+    }
 });
